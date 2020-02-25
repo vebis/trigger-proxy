b5146a1dc66697089246afc2b3b20c6e436abadc
vebis <vebis@gmx.net>
2020-02-25 15:10:08 +0100

    carve out some functions

--- assets/build/app.go
+++ assets/build/app.go
@@ -3,18 +3,25 @@ package main
 import (
 	"crypto/tls"
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
+const (
+	exitFail = 1
+)
 
-var mapping = make(map[string][]string)
-var quietPeriod int
-var timeKeeper = make(map[string]*time.Timer)
+var (
+	mapping = make(map[string][]string)
+	quietPeriod int
+	timeKeeper = make(map[string]*time.Timer)
+)
 
 func triggerJob(job string) bool {
 	ret := false
@@ -73,7 +80,6 @@ func createTimer(job string) {
 			delete(timeKeeper, job)
 		}
 	})
-	//    defer timer.Stop()
 
 	timeKeeper[job] = timer
 	if _, ok := timeKeeper[job]; ok {
@@ -91,6 +97,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if !ok || len(repos) < 1 {
 		log.Print("Repo is missing")
 		log.Print("Aborting request handling")
+
 		return
 	}
 
@@ -108,11 +115,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		branch = branchs[0]
 	}
 
-	log.Print("Parsed branch:", branch)
+	log.Print("Parsed branch: ", branch)
 
-	key := string(repo + "|" + branch)
+	key := BuildMappingKey([]string{repo, branch})
 
-	log.Print("Searching mappings for key:", key)
+	log.Print("Searching mappings for key: ", key)
 
 	if len(mapping[key]) < 1 {
 		fmt.Fprintf(w, "No mappings found")
@@ -121,7 +128,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Print("Number of mappings found:", len(mapping[key]))
+	log.Print("Number of mappings found: ", len(mapping[key]))
 
 	log.Print("Start processing mappings")
 	for _, job := range mapping[key] {
@@ -133,26 +140,31 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Print("Starting trigger-proxy ...")
+	if err := run(os.Args, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(exitFail)
+	}
+}
 
-	log.Print("Checking environment variables")
+func run(args []string, stdout io.Writer) error {
+	fmt.Fprintln(stdout, "Starting trigger-proxy ...")
+
+	fmt.Fprintln(stdout, "Checking environment variables")
 
 	if os.Getenv("JENKINS_URL") == "" {
-		log.Print("No JENKINS_URL defined")
-		return
+		return errors.New("No JENKINS_URL defined")
 	}
 
 	if os.Getenv("JENKINS_USER") == "" {
-		log.Print("No JENKINS_USER defined")
+		fmt.Fprintln(stdout, "No JENKINS_USER defined")
 	}
 
 	if os.Getenv("JENKINS_TOKEN") == "" {
-		log.Print("No JENKINS_TOKEN defined")
-		return
+		return errors.New("No JENKINS_TOKEN defined")
 	}
 
 	if os.Getenv("JENKINS_MULTI") != "" {
-		log.Print("Found multibranch project:", os.Getenv("JENKINS_MULTI"))
+		fmt.Fprintf(stdout, "Found multibranch project: %s\n", os.Getenv("JENKINS_MULTI"))
 	}
 
 	if os.Getenv("JENKINS_MULTI") != "" {
@@ -163,58 +175,63 @@ func main() {
 	if os.Getenv("JENKINS_QUIET") != "" {
 		tQuietPeriod, err := strconv.Atoi(os.Getenv("JENKINS_QUIET"))
 		if err != nil {
-			log.Fatal("Quiet Period could not be parsed. Aborting")
+			return errors.New("Quiet Period could not be parsed. Aborting")
 		}
 		quietPeriod = tQuietPeriod
-		log.Print("Found configured quiet period:", quietPeriod)
+		fmt.Fprintf(stdout, "Found configured quiet period: %s\n", quietPeriod)
 	}
 
-	log.Print("Project URL:", os.Getenv("JENKINS_URL"))
+	fmt.Fprintf(stdout, "Project URL: %s\n", os.Getenv("JENKINS_URL"))
 
 	mappingfile := "mapping.csv"
 	if os.Getenv("MAPPING_FILE") != "" {
 		mappingfile = os.Getenv("MAPPING_FILE")
-		log.Print("Found configured mapping file:", mappingfile)
+		fmt.Fprintf(stdout, "Found configured mapping file: %s\n", mappingfile)
 	}
 
-	log.Print("Reading mapping from file:", mappingfile)
-
-	file, err := os.Open(mappingfile)
-	if err != nil {
-		// err is printable
-		// elements passed are separated by space automatically
-		log.Print("Error:", err)
-		return
-	}
-	// automatically call Close() at the end of current method
-	defer file.Close()
-	//
-
-	reader := csv.NewReader(file)
-	// options are available at:
-	// http://golang.org/src/pkg/encoding/csv/reader.go?s=3213:3671#L94
-	reader.Comma = ';'
-	lineCount := 0
-	for {
-		// read just one record, but we could ReadAll() as well
-		record, err := reader.Read()
-		// end-of-file is fitted into err
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			log.Print("Error:", err)
-			return
-		}
-
-		mapping[record[0]+"|"+record[1]] = append(mapping[record[0]+"|"+record[1]], record[2])
-		lineCount += 1
+	if err := ParseMappingFile(mappingfile, stdout); err != nil {
+		return err
 	}
 
-	log.Print("Successfully read mappings:", lineCount)
-
-	log.Print("Adding handler")
 	http.HandleFunc("/", handler)
 
-	log.Print("Serving on port 8080")
+	fmt.Fprintln(stdout, "Serving on port 8080")
 	http.ListenAndServe(":8080", nil)
+
+	return nil
+}
+
+func ParseMappingFile(mappingfile string, stdout io.Writer) error {
+        fmt.Fprintf(stdout, "Reading mapping from file: %s\n", mappingfile)
+
+        file, err := os.Open(mappingfile)
+        if err != nil {
+                return err
+        }
+        defer file.Close()
+
+        reader := csv.NewReader(file)
+        reader.Comma = ';'
+        lineCount := 0
+        for {
+                record, err := reader.Read()
+
+                if err == io.EOF {
+                        break
+                } else if err != nil {
+                        return err
+                }
+
+		key := BuildMappingKey([]string{record[0],record[1]})
+                mapping[key] = append(mapping[key], record[2])
+                lineCount += 1
+        }
+
+	fmt.Fprintf(stdout, "Successfully read mappings: %d\n", lineCount)
+
+	return nil
+}
+
+func BuildMappingKey(keys []string) string {
+	return strings.Join(keys, "|")
 }
